c4a9070598505fe94d5a018fe6abfa6316c02176
Tanmaykk <tanmaykachroo@gmail.com>
2022-06-17 22:55:33 +0530

    Lots of refactoring, randomize, shuffle, download

--- src/index.js
+++ src/index.js
@@ -1,70 +1,32 @@
+import { draw, generateParticles, shuffle } from "./particles.js";
+
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 canvas.width = window.innerHeight / 1.4;
 canvas.height = canvas.width;
 
-ctx.font = "14px serif";
-ctx.fillStyle = "#000";
-ctx.globalCompositeOperation = "saturation";
-function random_rgb() {
-  var o = Math.round,
-    r = Math.random,
-    s = 255;
-  return [o(r() * s), o(r() * s), o(r() * s)];
-}
-const particles = new Array(25).fill(0).map((_) => {
-  return {
-    x: canvas.width * Math.random(),
-    y: canvas.height * Math.random(),
-    // rgb: random_rgb(),
-    hsl: [~~(360 * Math.random()), 70, 80],
-    vx: 2 * Math.random(),
-    vy: 7 * Math.random(),
-    radius: Math.max(600, Math.random() * 700),
-  };
+context.font = "14px serif";
+context.fillStyle = "#000";
+context.globalCompositeOperation = "saturation";
+
+const download = () => {
+  const link = document.createElement("a");
+  link.download = "mesh-gradient.png";
+  link.href = canvas.toDataURL();
+  link.click();
+};
+document.getElementById("download").addEventListener("click", download);
+
+document.getElementById("randomize").addEventListener("click", () => {
+  generateParticles(canvas, 25);
 });
 
-const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+document.getElementById("shuffle").addEventListener("click", shuffle);
 
-  // draw something
-
-  particles.forEach((particle) => {
-    const grad = ctx.createRadialGradient(
-      particle.x,
-      particle.y,
-      1,
-      particle.x,
-      particle.y,
-      particle.radius
-    );
-    const [h, s, l] = particle.hsl;
-    grad.addColorStop(0, `hsla(${h},${s}%,${l}%,1)`);
-    grad.addColorStop(1, `hsla(${h},${s}%,${l}%,0)`);
-    ctx.beginPath();
-    ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
-    ctx.fillStyle = grad;
-    ctx.fill();
-
-    // particle.radius = particle.radius
-    if (
-      particle.x - particle.radius / 2 >= canvas.width ||
-      particle.x + particle.radius / 2 < 0
-    )
-      particle.vx *= -1;
-    if (
-      particle.y - particle.radius / 2 >= canvas.height ||
-      particle.y + particle.radius / 2 < 0
-    )
-      particle.vy *= -1;
-    particle.x += particle.vx;
-    particle.y += particle.vy;
-  });
-};
-
+generateParticles(canvas, 25);
 requestAnimationFrame(animate);
 function animate() {
   // request another loop of animation
-  draw();
+  draw(canvas, context);
   requestAnimationFrame(animate);
 }
---
+++ src/particles.js
@@ -0,0 +1,60 @@
+let particles = [];
+const randomLocation = () => {
+  return { x: canvas.width * Math.random(), y: canvas.height * Math.random() };
+};
+export const generateParticles = (canvas, numberOfParticles) => {
+  particles = new Array(numberOfParticles).fill(0).map((_) => {
+    return {
+      ...randomLocation(),
+      hsl: [~~(360 * Math.random()), 70, 80],
+      vx: 2 * Math.random(),
+      vy: 7 * Math.random(),
+      radius: Math.max(600, Math.random() * 650),
+    };
+  });
+};
+
+export const draw = (canvas, context) => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach((particle) => {
+    const grad = context.createRadialGradient(
+      particle.x,
+      particle.y,
+      1,
+      particle.x,
+      particle.y,
+      particle.radius
+    );
+    const [h, s, l] = particle.hsl;
+    grad.addColorStop(0, `hsla(${h},${s}%,${l}%,1)`);
+    grad.addColorStop(1, `hsla(${h},${s}%,${l}%,0)`);
+    context.beginPath();
+    context.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
+    context.fillStyle = grad;
+    context.fill();
+
+    // particle.radius = particle.radius
+    if (
+      particle.x - particle.radius / 2 >= canvas.width ||
+      particle.x + particle.radius / 2 < 0
+    )
+      particle.vx *= -1;
+    if (
+      particle.y - particle.radius / 2 >= canvas.height ||
+      particle.y + particle.radius / 2 < 0
+    )
+      particle.vy *= -1;
+    particle.x += particle.vx;
+    particle.y += particle.vy;
+  });
+};
+
+export const shuffle = () => {
+  particles.forEach((particle) => {
+    const { x, y } = randomLocation();
+    particle.x = x;
+    particle.y = y;
+    particle.radius = Math.max(600, Math.random() * 650);
+  });
+};
